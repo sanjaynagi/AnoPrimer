6fdfdbe77d8ae5bbe0ab2abb3c68ad8d692bd742
Sanjay C Nagi <sanjay.c.nagi@gmail.com>
2022-07-07 13:52:56 +0100

    change to AgamPrimer

---
+++ AgamPrimer/__init__.py
@@ -0,0 +1,2 @@
+from . import AgamPrimer
+from .AgamPrimer import *
--- setup.py
+++ setup.py
@@ -4,23 +4,22 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="agamPrimer",
-    version="0.1.6",
+    name="AgamPrimer",
+    version="0.2.0",
     author="Sanjay Curtis Nagi",
     author_email="sanjay.c.nagi@gmail.com",
     description="A small package to store some functions for the PrimerDesignAg notebook, to improve readability.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sanjaynagi/PrimerDesignAg",
+    url="https://github.com/sanjaynagi/AgamPrimer",
     project_urls={
-        "Bug Tracker": "https://github.com/sanjaynagi/PrimerDesignAg/issues",
+        "Bug Tracker": "https://github.com/sanjaynagi/AgamPrimer/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-   # package_dir={"": "agamPrimer"},
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
